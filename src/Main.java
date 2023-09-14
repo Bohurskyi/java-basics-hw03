c94a3c619115e62fa28bf36bd6d6a72b4c427871
Dmytro Bohurskyi <bogurskiy96@gmail.com>
2023-09-14 20:42:03 +0300

    java-basics-hw03 OOP1 done

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student("Dmytro", 100);
+
+        Student student2 = new Student();
+        student2.setName("Denys");
+        student2.setRating(90);
+
+        Student student3 = new Student("Ihor", 10);
+
+        double average = Student.getAvgRating(student1, student2, student3);
+        System.out.println("Avarage = " + average);
+
+        student1.changeRating(20);
+
+        average = Student.getAvgRating(student1, student2, student3);
+        System.out.println("Avarage = " + average);
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,50 +1,61 @@
 public class Student {
-  int rating;
-  private String name;
+    private int rating;
+    private String name;
 
-  // TODO implement Student class according to the instructions provided in the README.md file
+    public Student() {
 
-  public Student(String name) {
-    //TODO initialize name
-  }
+    }
 
-  public static double getAvgRating() {
-    // TODO return average rating of all students
-    return 0;
-  }
+    public Student(String name) {
+        this.name = name;
+    }
+    public Student(String name, int rating) {
+        this.name = name;
+        this.rating = rating;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public static double getAvgRating(Student student1, Student student2, Student student3) {
+        double avarage = (student1.getRating()+student2.getRating()+student3.getRating())/3;
+        return avarage;
+    }
 
-  public void setName(String name) {
-    // TODO set student's name
-  }
+    public static double getAvgRating() {
+        return 0;
+    }
 
-  public int getRating() {
-    return rating;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setRating(int rating) {
-    // TODO initialize rating;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public boolean betterStudent(Student student) {
-    // TODO return the result of comparing this.student's rating with the student's rating
-    return false;
-  }
+    public int getRating() {
+        return rating;
+    }
 
-  public void changeRating(int rating) {
-    // TODO change this student's rating and average rating of all students
-  }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 
-  public static void removeStudent(Student student) {
-    // TODO remove student
-  }
+    public boolean betterStudent(Student student) {
+        // TODO return the result of comparing this.student's rating with the student's rating
+        return this.rating < student.getRating();
+    }
 
-  @Override
-  public String toString() {
-    // TODO return String with name and rating of this student
-    return "";
-  }
+    public void changeRating(int rating) {
+        // TODO change this student's rating and average rating of all students
+        this.rating = rating;
+    }
+
+    public static void removeStudent(Student student) {
+        // TODO remove student
+    }
+
+    @Override
+    public String toString() {
+        // TODO return String with name and rating of this student
+        return "my name: " + name + ", my rating: " + rating;
+    }
 }
